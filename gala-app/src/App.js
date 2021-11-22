d3fc8d5d88c59f11b0f0c7d103146694075a9667
robintan98 <robin.tan98@gmail.com>
2021-11-22 14:42:57 -0500

    started basic frontend

--- gala-app/src/App.js
+++ gala-app/src/App.js
@@ -5,18 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Gala.
         </p>
-        <a
+        <br /> <br /> <br />
+        <div>
+          <button>
+            Login
+          </button>
+        </div>
+        <div>
+          <button>
+            Create Account
+          </button>
+        </div>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
