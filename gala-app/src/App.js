6ceca99411dde67b716d88eac4f82846db2fdd09
robintan98 <robin.tan98@gmail.com>
2022-01-03 01:45:41 -0800

    created signup page

--- gala-app/src/App.js
+++ gala-app/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import Header from './components/Header';
-import Body from './components/Body';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Signup from './components/Signup';
 import './css/App.css';
 
 
 function App() {
   return (
     <React.Fragment>
-      <Header />
-      <Body />
+      <BrowserRouter>
+        <Routes >
+          <Route exact path="" element={<Homepage />} />
+          <Route exact path="/signup" element={<Signup/>} />
+        </Routes>
+      </BrowserRouter>
     </React.Fragment>
     
   );
---
+++ gala-app/src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Body from '../components/Body';
+import '../css/Homepage.css';
+
+
+function Homepage() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Body />
+    </React.Fragment>
+    
+  );
+}
+
+export default Homepage;
