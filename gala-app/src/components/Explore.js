137e193368df73224b08459b49555d5e73ece12f
robintan98 <robin.tan98@gmail.com>
2022-01-07 20:53:26 -0800

    worked on nav stuff for explore

--- gala-app/src/components/Explore.js
+++ gala-app/src/components/Explore.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import Navigation from './Navigation';
 import '../css/Explore.css';
 
 function Explore() {
   return (
     <React.Fragment>
-      <div className="ExploreArea">
-        {/* explore */}
+      <UserHeader />
+      <div className="DashboardArea">
+        <Navigation selectedOption="explore"/>
+        <div className="ExploreArea">
+          explore stuff
+        </div>
       </div>
     </React.Fragment>
     
--- gala-app/src/components/Navigation.js
+++ gala-app/src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowRight, faChevronRight} from '@fortawesome/free-solid-svg-icons'
 import '../css/Navigation.css';
 
-function Navigation() {
+function Navigation(props) {
   const [shouldCollapse, setShouldCollapse] = useState(false);
 
   const handleClick = () => {
     setShouldCollapse(!shouldCollapse);
-}
+  }
   return (
     <React.Fragment>
       {shouldCollapse ? <div className="NavigationAreaCollapsed">
@@ -30,6 +30,68 @@ function Navigation() {
               </div>
             </div>
           </div>
+        </div>
+        <div className="OptionsArea">
+          {props.selectedOption == "explore" ?
+            <div className = "OptionSelected">
+              <div className="OptionSelectedSide" />
+                <div>
+                  EXPLORE
+                </div>
+                <div className="OptionSelectedSide">
+                <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+            </div>
+            :
+            <div className = "Option">
+              EXPLORE
+          </div>
+          }
+          {props.selectedOption == "create" ?
+            <div className = "OptionSelected">
+              <div className="OptionSelectedSide" />
+                <div>
+                  CREATE
+                </div>
+                <div className="OptionSelectedSide">
+                <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+            </div>
+            :
+            <div className = "Option">
+              CREATE
+          </div>
+          }
+          {props.selectedOption == "messages" ?
+            <div className = "OptionSelected">
+              <div className="OptionSelectedSide" />
+                <div>
+                  MESSAGES
+                </div>
+                <div className="OptionSelectedSide">
+                <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+            </div>
+            :
+            <div className = "Option">
+              MESSAGES
+          </div>
+          }
+          {props.selectedOption == "archive" ?
+            <div className = "OptionSelected">
+              <div className="OptionSelectedSide" />
+                <div>
+                  ARCHIVE
+                </div>
+                <div className="OptionSelectedSide">
+                <FontAwesomeIcon icon={faChevronRight}/>
+              </div>
+            </div>
+            :
+            <div className = "Option">
+              ARCHIVE
+          </div>
+          }
         </div>
       </div>}
       
