d8c3230298508b01fe1c603855ef2ab9d54736a2
robintan98 <robin.tan98@gmail.com>
2022-01-11 07:35:19 -0500

    made date picker for explore dashbooard

--- gala-app/src/components/Explore.js
+++ gala-app/src/components/Explore.js
@@ -5,10 +5,16 @@ import ExploreEntry from './ExploreEntry';
 import ReactAnime from 'react-animejs';
 import '../css/Dashboard.css';
 import '../css/Explore.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 const { Anime } = ReactAnime;
 
 function Explore() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   // controller state
   const [control, setControl] = useState(null);
 
@@ -50,25 +56,36 @@ function Explore() {
             </div>
           </div>
           <div className="ExploreToolbarArea">
-            <input className="ExploreToolbarSearch" placeholder="Search by keyword">
-            </input>
-            <select name="explore-distance" id="explore-distance" className="ExploreToolbarSelect">
-              <option value="none" selected disabled hidden>Location</option>
-              <option value="one_mile">1 mile</option>
-              <option value="five_miles">5 miles</option>
-              <option value="ten_miles">10 miles</option>
-              <option value="fifty_miles">50 miles</option>
-            </select>
-            <select name="explore-sort" id="explore-sort" className="ExploreToolbarSelect">
-              <option value="none" selected disabled hidden>Sort by</option>
-              <option value="most_recent">Most recent</option>
-              <option value="least_recent">Least recent</option>
-              <option value="most_recent">Earliest</option>
-              <option value="least_recent">Latest</option>
-              <option value="most_recent">Highest $$$</option>
-              <option value="least_recent">Lowest $</option>
-              <option value="most_relevant">Optimized for you</option>
-            </select>
+            <div className="ExploreToolbarLeftArea">
+              <input className="ExploreToolbarSearch" placeholder="Search by keyword">
+              </input>
+              <div className="DatePickerArea">
+                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                <div className="DatePickerBetweenText">
+                  to
+                </div>
+                <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+              </div>
+            </div>
+            <div className="ExploreToolbarRightArea">
+              <select name="explore-distance" id="explore-distance" className="ExploreToolbarSelect">
+                <option value="none" selected disabled hidden>Distance</option>
+                <option value="one_mile">1 mile</option>
+                <option value="five_miles">5 miles</option>
+                <option value="ten_miles">10 miles</option>
+                <option value="fifty_miles">50 miles</option>
+              </select>
+              <select name="explore-sort" id="explore-sort" className="ExploreToolbarSelect">
+                <option value="none" selected disabled hidden>Sort by</option>
+                <option value="most_recent">Most recent</option>
+                <option value="least_recent">Least recent</option>
+                <option value="most_recent">Earliest</option>
+                <option value="least_recent">Latest</option>
+                <option value="most_recent">Highest $$</option>
+                <option value="least_recent">Lowest $$</option>
+                <option value="optimized">Optimized</option>
+              </select>
+            </div>
           </div>
           <div className="ExploreEntryArea">
             <ExploreEntry />
