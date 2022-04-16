b7950f610318c9bd28a5676bf28b667b02e1db3d
Kanishka Ragula <k.ragula1@gmail.com>
2022-04-15 19:17:11 -0600

    Added distance to my dates section

--- gala-frontend/src/axios/dates.js
+++ gala-frontend/src/axios/dates.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
+
 import serverConfig from '../configurations/server-config.js';
 import routeExperience from '../configurations/route-experience-config.js';
 
-export const getDates = async () => {
+export const getDates = async locationData => {
 	// todo: remove hardcode email
+	let searchParams = new URLSearchParams(locationData);
+	console.log(locationData)
+
+
 	return await axios
-		.get(`${serverConfig.backendURL}/experience/dates`, {
+		.get(`${serverConfig.backendURL}/experience/dates?${searchParams.toString()}`, {
 			withCredentials: true,
 			headers: {
 				'x-access-token': localStorage.getItem('token'),
